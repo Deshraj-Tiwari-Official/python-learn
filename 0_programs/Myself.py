f9e97fb705c40cbaad1581405c94337cb4b26d61
Deshraj-Tiwari <deshrajtiwari690@gmail.com>
2024-07-06 18:07:05 +0530

    Myself program

---
+++ 0_programs/Myself.py
@@ -0,0 +1,5 @@
+name=input("Enter your name: ")
+role=input("Enter your role: ")
+age=int(input("Enter your age:"))
+
+print(f"Hello there {name}, Mr {role}.\nLemme guess your age, is it {age}??\n\tI bet ya it is.")
