78086635d23c30e82b523a7c022b7737d497ac91
Deshraj-Tiwari <deshrajtiwari690@gmail.com>
2024-07-09 16:38:41 +0530

    Chapter7 Loops

---
+++ Chapter7/for_loop_iterate.py
@@ -0,0 +1,14 @@
+# Iterating Lists
+l1 = [1, 4, 6, 254, 6, 74]
+for i in l1:
+    print(i)
+
+# Iterating Tuples
+t1 = (1, 4, 6, 254, 6, 74)
+for i in t1:
+    print(i)
+
+# Iterating Strings
+s1 = "Deshraj"
+for i in s1:
+    print(i)
---
+++ Chapter7/for_loop_with_else.py
@@ -0,0 +1,6 @@
+# We can create for loops with else statement that will execute when the loop is exited
+
+for i in range(0, 11):
+    print(i)
+else:
+    print("Loop completed")
